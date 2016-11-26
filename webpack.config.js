90b9f6d8ed8ddb963e5d176da5431c61737dd718
Jorge <jcarapia83@Gmail.com>
2016-11-26 10:03:26 -0800

    deployment

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: [
-  'script!jquery/dist/jquery.min.js',
     './src/index.js'
   ],
   output: {
@@ -11,27 +10,6 @@ module.exports = {
     publicPath: '/',
     filename: 'bundle.js'
   },
-  externals: {
-		jquery: 'jQuery'
-	},
-	plugins: [
-		new webpack.ProvidePlugin({
-			'$': 'jquery',
-			'jQuery': 'jquery'
-		})
-	],
-	resolve:{
-		root: __dirname,
-		modulesDirectories: [
-			'node_modules',
-			'./src/components',
-			'./style'
-		],
-		alias: {
-			applicationStyles: 'style/style.scss'
-		},
-		extensions: ['', '.js', '.jsx']
-	},  
   module: {
     loaders: [{
       exclude: /node_modules/,
