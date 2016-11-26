a508c182d5d5ebdd25bd6652eb676cc0ba8709fb
Jorge <jcarapia83@Gmail.com>
2016-11-26 15:13:33 -0800

    root url issues with heroku

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {browserHistory} from 'react-router';
 import {AUTH_USER, AUTH_ERROR, UNAUTH_USER, FETCH_TALES} from './types';
 
-const ROOT_URL = "http://localhost:8080" || 'http://pacific-fjord-58728.herokuapp.com/signup'; 
+const ROOT_URL = 'http://pacific-fjord-58728.herokuapp.com'; 
 
 export function signupUser({username, email, password}){
 	console.log('signupUser(): ',username, email, password)
