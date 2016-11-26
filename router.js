256bb6f534dc3a2269ecb046d1186d72af2dcd8a
Jorge <jcarapia83@Gmail.com>
2016-11-26 15:06:27 -0800

    debugging deployment

--- router.js
+++ router.js
@@ -8,9 +8,9 @@ const requireSignin = passport.authenticate('local', {session: false});
 
 module.exports = function(app){
 
-	app.get('/', requireAuth, function(req, res) {
-		res.send({message: 'super secret code is abc123'});
-	});
+	// app.get('/', requireAuth, function(req, res) {
+	// 	res.send({message: 'super secret code is abc123'});
+	// });
 
 	app.post('/signin', requireSignin, Authentication.signin);
 
