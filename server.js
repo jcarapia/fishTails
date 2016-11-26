af128d3144076d40c37350d0c92fe9c86e36b48a
Jorge <jcarapia83@Gmail.com>
2016-11-25 16:57:41 -0800

    debugging deployment

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-var express = require('express');
-var path = require('path');
-
-var app = express();
-
-var PORT = process.env.PORT || 3000;
+const express = require('express');
+const path = require('path');
+const port = 8080;
+const app = express();
 
 app.use(express.static(__dirname));
 
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'index.html'))
+app.get('/api/posts', (req, res) => {
+  res.send([{ id: 1 }]);
 });
 
-app.listen(PORT, function(){
-	console.log('the magic happens on port: ' + PORT);
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
+
+app.listen(port);
+console.log('Server started');
