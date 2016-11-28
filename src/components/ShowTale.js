e51bbdbec0ab7c41f2417a80e78926552597fca4
Jorge <jcarapia83@Gmail.com>
2016-11-27 18:03:30 -0800

    styling showTale page

--- src/components/ShowTale.js
+++ src/components/ShowTale.js
@@ -3,13 +3,35 @@ import {connect} from 'react-redux';
 import * as actions from '../actions';
 
 class ShowTale extends Component {
+
+
 	render() {
+
+		//const tale = "https://farm3.staticflickr.com/2269/1621457215_6d071e57ba.jpg";
+		const tale = "https://farm5.staticflickr.com/4078/4859517719_f8a9aba7dc.jpg";
+		const title = 'The title';
+		const author = 'Joe Blow';
+		const story = "blah blah blah";
+
 		return(
-			<div>
-				The tale Component
+			<div className="container showTale">
+				<div className="row">
+					<div className="col-sm-12">
+						<div className="thumbnail">
+							<img className="image-responsive" src={tale}/>
+							<div className="caption-full">
+								<h4>{title}</h4>
+								<p>{story}</p>
+								<p><em>Submitted by: {author}</em></p>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		)
 	}
 };
 
 export default ShowTale;
+
+
